841f312c4849929c59fa12fcfd652ca3cc48a58c
alinazarif <alinazarif06@gmail.com>
2024-09-29 22:45:52 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+  return(
+    <div className="homeContainer">
+      <div className="childContainer">
+        Hey,This is ALINAZ ARIF I'm a next.js developer </div>
+      </div>
+  )
+}
